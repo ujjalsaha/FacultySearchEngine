0a9514c444ac98a7d47eb4da72360ef265df6151
sudiptobilu <SU_DI_PTO@YAHOO.COM>
2021-12-07 18:27:22 -0500

    Latest Search function update

--- apps/backend/api/search.py
+++ apps/backend/api/search.py
@@ -1,20 +1,28 @@
 from apps.backend.utils.facultydb import FacultyDB
 from apps.backend.utils.ranker import Ranker
-class search:
+class Search:
     def __init__(self):
         print("Initialize")
 
-    def dosearch(self, university_filter, location_filter):
-        print("Corpus Method")
-        faculty = FacultyDB()
-        list = faculty.get_biodata_records(university_filter, location_filter)
-        print(list)
+    def get_search_results(self, university_filter, location_filter):
+        try:
+            # print("Corpus Method")
+            corpus = FacultyDB().get_biodata_records(university_filter, location_filter)
+            #print(corpus)
 
-        ranker = Ranker(list)
-        rankedlist = ranker.score("ANOOP BN", 10)
-        print(rankedlist)
+            ranked_id_list = Ranker(corpus).score("ANOOP BN", 10)
+            #print(ranked_id_list)
+
+            search_results_list  = FacultyDB().get_faculty_records(ranked_id_list)
+            print(search_results_list )
+
+            return search_results_list
+
+        except Exception as e:
+            print(f"Unexpected exception encountered: {e}")
+            return []
 
 if __name__ == '__main__':
-    print("Main")
-    search = search()
-    search.dosearch("Manipal","Sikkim")
+    # print("Main")
+    search = Search()
+    search.get_search_results("Manipal","Sikkim")
